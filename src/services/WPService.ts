33f438570bfb16187bddffa0c0b65452da3cc8de
Fernando Avila <fernandoavilajunior@gmail.com>
2025-02-14 00:03:01 -0300

    wp service get post fix

--- src/services/WPService.ts
+++ src/services/WPService.ts
@@ -12,6 +12,6 @@ const api: AxiosInstance = axios.create({
 });
 
 export const get_posts = (): Promise<Post[]> => api.get<PostDTO[]>('/wp/v2/posts', { params: { _embed: "wp:featuredmedia,author,wp:term" } }).then( response => response.data ? MapPosts(response.data) : []);
-export const get_post = (slug:string): Promise<Post> => api.get<PostDTO>('/wp/v2/posts', { params: { slug: slug, _embed: "wp:featuredmedia,author,wp:term" } }).then( response => MapPost(response.data) );
+export const get_post = (slug:string): Promise<Post> => api.get<PostDTO[]>('/wp/v2/posts', { params: { slug: slug, _embed: "wp:featuredmedia,author,wp:term" } }).then( response => MapPost(response.data[0]) );
 export const get_page = (slug:string): Promise<Page> => api.get<PageDTO>('/wp/v2/pages', { params: { slug: slug } }).then( response => MapPage(response.data) );
 export const get_settings = (): Promise<SiteSettings> => api.get<SiteSettings>('/blog-theme/v1/info').then( response =>response.data );
